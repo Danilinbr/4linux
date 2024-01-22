4b6a524c13a853f783115608d3acd1c8cfc2f1fa
Danilinbr <danilin.jovem@gmail.com>
2024-01-22 17:47:15 -0300

    Add files via upload

---
+++ aula_01_exercicios.py
@@ -0,0 +1,21 @@
+# 1) Em muitos programas, nos é solicitado o preenchimento de algumas informações como
+# nome, CPF, idade e unidade federativa. Escreva um script em Python que solicite as
+# informações cadastrais mencionadas e que em seguida as apresente da seguinte forma:
+
+# -----------------------------
+# Confirmação de cadastro:
+# Nome: Guido Van Rossum
+# CPF: 999.888.777/66
+# Idade: 65
+# -----------------------------
+
+# 2) Escreva um script em Python que receba dois números e que seja realizado as seguintes
+# operações:
+# • soma dos dois números
+# • diferença dos dois números
+# O resultado deverá ser apresentado conforme a seguir - no exemplo foram digitados os números
+# 4 e 2:
+
+# ------------------------------
+# Soma: 4 + 2 = 6
+# Diferença: 4 - 2 = 2
